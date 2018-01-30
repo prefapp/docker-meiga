22557da4c652a6146ceea99ad25c7574968bb0a3
Francisco Maseda Muiño <frmadem@gmail.com>
2018-01-30 14:12:25 +0100

    Modificadas variables de entorno

--- lib/error.php
+++ lib/error.php
@@ -42,7 +42,7 @@
             <div class="row">
                 <div class="col-2"></div>
                 <div class="col-8">
-                    <p class="titulo error">Error</p>
+                    <p class="titulo error">Erro</p>
                 </div>
             </div>
 
